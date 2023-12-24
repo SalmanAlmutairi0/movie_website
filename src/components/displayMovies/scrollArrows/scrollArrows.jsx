f37ebe9c8ed790b18575ddd97d4350962b8a40b9
salman almutairi <t123y2@gmail.com>
2023-12-24 20:35:08 +0300

    fixed the scroll Arrows

--- src/components/displayMovies/scrollArrows/scrollArrows.jsx
+++ src/components/displayMovies/scrollArrows/scrollArrows.jsx
@@ -1,17 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function ScrollArrows({Ref,arrow}) {
+  const [visibleArrow, setVisibleArrow] = useState({left: false, right: true});
     const handleScroll = (direction) => {
         const container = Ref.current;
     
         if (container) {
-          const scrollAmount = 1100; 
+          const scrollAmount = 1200; 
           const currentScroll = container.scrollLeft;
-          console.log(container.scrollLeft);
+          // scrollWidth is the total width of the content
+          // clientWidth is the visible width of the content
+          const maxScroll = container.scrollWidth - container.clientWidth
     
           if (direction === 'left') {
-            container.scrollLeft = currentScroll - scrollAmount;
-          } else {
+            // check if the user scrolled to the end of the content 
+            if(currentScroll - scrollAmount < 0) {
+                setVisibleArrow({left: false, right: true});
+            }else{
+                setVisibleArrow({left: true, right: true});
+            }
+            // container.scrollLeft = currentScroll - scrollAmount
+            container.scrollLeft = Math.max(0, currentScroll - scrollAmount);
+
+
+          } else if(direction === 'right') {
+            // check if the user scrolled to the start of the content 
+
+            if(currentScroll + scrollAmount > maxScroll) {
+                setVisibleArrow({left: true, right: false});
+            }else{
+                setVisibleArrow({left: true, right: true});
+
+            }
             container.scrollLeft = currentScroll + scrollAmount;
           }
         }
@@ -21,11 +41,13 @@ export default function ScrollArrows({Ref,arrow}) {
 
   return (
     <>
-    <div className="arrow-icon-right" onClick={() => handleScroll('right')}>
+    <div className="arrow-icon-right" style={{display: visibleArrow.right ? 'block' :  'none'}}
+            onClick={() => handleScroll('right')}>
             <img src={arrow} alt="arrow right" />
           </div>
 
-          <div className="arrow-icon-left" onClick={() => handleScroll('left')}>
+          <div className="arrow-icon-left" style={{display: visibleArrow.left ? 'block' :  'none'}}
+              onClick={() => handleScroll('left')}>
             <img src={arrow} alt="arrow right" />
           </div>
     </>
